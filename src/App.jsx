fb0e0862190755fbb13b5e20e9a1ae4335511be1
Surya Annadurai <suryaannadurai1041999@gmail.com>
2025-02-19 23:18:38 +0530

    updated

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+// import './App.css'
+import Nav from './nav/nav'
+import Footer from './footer/footer'
+import MyComponents from './myComponents/myComponents'
+// import styles from './App.module.css'
+
+function App() {
+ 
+
+  return (
+    <>
+    <div>
+    <Nav />
+     <MyComponents />
+     <Footer />
+    </div>
+   
+    </>
+  )
+}
+
+export default App
